44ce95ac501c497703866c4bf924d008527df99e
Fatemeh <fatemeh.engqvist@gmail.com>
2018-10-10 21:01:40 +0200

    final version with documentation

--- src/com/sda/todolist/model/Task.java
+++ src/com/sda/todolist/model/Task.java
@@ -2,19 +2,26 @@ package com.sda.todolist.model;
 
 import java.time.LocalDate;
 
-
+/**
+ * This class is a part of "ToDoList" project.
+ * A single task which is created by TaskManager class.
+ *
+ * @author Fatemeh Engqvist
+ */
 public class Task implements Comparable<Task> {
 
     private String title;
     private LocalDate dueDate;
     private String project;
     private boolean isDone;
-    private static int lastId = 0;
     private int id;
 
-    public Task(String title, LocalDate dueDate, String project, boolean isDone) {
-        lastId ++;
-        id = lastId;
+
+    /**
+     * Constructor of the task takes title, due date, project, status and id number.
+     */
+    public Task(String title, LocalDate dueDate, String project, boolean isDone, int id) {
+        this.id = id;
         this.title = title;
         this.dueDate = dueDate;
         this.project = project;
@@ -42,39 +49,44 @@ public class Task implements Comparable<Task> {
         return isDone;
     }
 
-    public void setTitle(String title) {
+     void setTitle(String title) {
         this.title = title;
     }
 
-    public void setDueDate(LocalDate dueDate) { this.dueDate = dueDate;
+     void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
     }
 
-    public void setProject(String project) {
+     void setProject(String project) {
         this.project = project;
     }
 
-    public void setDone(boolean done) {
+     void setDone(boolean done) {
         isDone = done;
     }
 
-    public void updateTask(String title, LocalDate dueDate, String project, boolean isDone) {
-        setTitle(title);
-        setDueDate(dueDate);
-        setProject(project);
-        setDone(isDone);
-    }
-
+    /**
+     * Compares this task with the specified task for order using due date.
+     * Returns a negative, zero, or a positive integer as this.task due date is before, equal, after the specified task.
+     * @param t task
+     * @return  negative, zero, or a positive integer as this.task due date is before, equal, after the specified task.
+     */
+    @Override
     public int compareTo(Task t) {
         return dueDate.compareTo(t.getDueDate());
     }
 
+    /**
+     * Returns a task in String format.
+     * @return task
+     */
     @Override
     public String toString() {
         String status = "task is Not Done";
-        if(isDone) {
+        if (isDone) {
             status = "task is Done";
         }
-        return  "Task id: " + id + "  Title: " + title + "  Due date: " +
-                dueDate + "  Project: " + project + "  Status: " + status ;
+        return "Task id: " + id + "  Title: " + title + "  Due date: " +
+                dueDate + "  Project: " + project + "  Status: " + status;
     }
 }
